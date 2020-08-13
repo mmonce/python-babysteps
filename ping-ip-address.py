3d35c67a66acc45658565bc504ee5be8211ee598
Mark Monce <mmonce@vmware.com>
2020-08-12 20:46:15 -0500

    Corrected the right script this time single word

--- ping-ip-address.py
+++ ping-ip-address.py
@@ -24,7 +24,7 @@ pingstatus = check_ping()
 print (pingstatus)
 
 #WRITE PINGSTATUS TO A NEW FILE THAT CAN BE STORED AS AN ENV VARIABLE I THE LINUX SHELL AND BOUND TO OUTPUT
-pythonoutputfile = open("ping_ip_output.txt","a+")
+pythonoutputfile = open("ping_ip_results.txt","a+")
 pythonoutputfile.write(pingstatus)
 pythonoutputfile.close
 
